cde3c912e753f69be36d84dd47ad4667829c1c59
Hunter Rancourt <hunter.rancourt@gmail.com>
2020-09-04 15:52:00 -0500

    feat: Add test method with doc block

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -26,4 +26,20 @@ class UserTest extends TestCase {
 		// Test what happens when no values are assigned
 		$this->assertEquals('', $user->getFullName());
 	}
+
+	/**
+	 * With the doc block have "@test" PHPUnit knows this is a test method
+	 * If a method does not have a doc block with "@test" and the function isn't prefaced with test PHPUnit will not know to run it
+	 * @test
+	 */
+	public function user_has_first_name() {
+		// Instantiate User class
+		$user = new User;
+
+		// Set User's first name
+		$user->first_name = 'Alvin';
+
+		// Test if first name matches
+		$this->assertEquals('Alvin', $user->first_name);
+	}
 }
