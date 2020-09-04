cf205c146037a6ef84d27ca41e7c611a1c79ae9a
Hunter Rancourt <hunter.rancourt@gmail.com>
2020-09-04 15:13:52 -0500

    feat: Add first test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase {
+	public function testAddingTwoPlusTwoResultsFour() {
+		$this->assertEquals(4, 2 + 2);
+	}
+}
