63d105083929dac0af961cf46cecab109ab8342c
Hunter Rancourt <hunter.rancourt@gmail.com>
2020-09-10 15:22:54 -0500

    feat: Add ability to get an article's slug

--- src/App/Article.php
+++ src/App/Article.php
@@ -7,4 +7,16 @@ class Article {
 	 * @var string $title The article's title
 	 */
 	public $title;
+
+	/**
+	 * Get the article's slug
+	 *
+	 * @return string The article's slug
+	 */
+	public function getSlug(): string {
+		// Based off the tests written in ArticleTest.php this is enough
+		// code to make the test pass so we can move on and come back later
+		// to add the actual logic that converts the title to a url friendly slug
+		return '';
+	}
 }
--- tests/ArticleTest.php
+++ tests/ArticleTest.php
@@ -10,4 +10,12 @@ class ArticleTest extends TestCase {
 		// Assert the title property will be null or empty
 		$this->assertEmpty($article->title);
 	}
+
+	public function testSlugIsEmptyWithNoTitle() {
+		// Instantiate the Article class
+		$article = new App\Article;
+
+		// Assert the article's slug is identical to an empty string
+		$this->assertSame($article->getSlug(), '');
+	}
 }
