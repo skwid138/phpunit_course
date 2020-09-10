9ac1bf69c5282413b44328a9a66d69ea7e1bc258
Hunter Rancourt <hunter.rancourt@gmail.com>
2020-09-10 11:49:06 -0500

    test: Impliment Mockery

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,8 +1,9 @@
 <?php declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 
-class ExampleTest extends TestCase {
+class ExampleTest extends MockeryTestCase {
 	public function testAddingTwoPlusTwoResultsFour() {
 		$this->assertEquals(4, 2 + 2);
 	}
